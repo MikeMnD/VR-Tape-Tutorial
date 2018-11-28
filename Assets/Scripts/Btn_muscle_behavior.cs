8fd0e453c278cafe63c0338a4a9614afd7861fde
cyj407 <dfghj345678@gmail.com>
2018-11-28 18:00:44 +0800

    Initial commit

---
+++ Assets/Scripts/Btn_muscle_behavior.cs
@@ -0,0 +1,30 @@
+﻿namespace VRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class Btn_muscle_behavior : VRTK_InteractableObject
+    {
+        public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+        {
+            base.StartUsing(currentUsingObject);
+            Debug.Log("go to next scene");
+            /* change scene */
+            //SceneManager.LoadScene(this.name);
+            SceneManager.LoadScene("test");
+            StopUsing();
+        }
+
+        protected void Start()
+        {
+
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
---
+++ Assets/Scripts/Customized_Pointer.cs
@@ -0,0 +1,39 @@
+﻿namespace VRTK {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Customized_Pointer : VRTK_Pointer
+    {
+        private Transform headset;
+        private GameObject _tooltip;
+
+        public override void OnDestinationMarkerEnter(DestinationMarkerEventArgs e)
+        {
+            base.OnDestinationMarkerEnter(e);
+            Transform _btn = e.target;
+            _tooltip = _btn.GetChild(0).gameObject;
+            _tooltip.SetActive(true);
+        }
+
+        public override void OnDestinationMarkerExit(DestinationMarkerEventArgs e)
+        {
+            base.OnDestinationMarkerHover(e);
+            if(_tooltip != null) {
+                _tooltip.SetActive(false);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
