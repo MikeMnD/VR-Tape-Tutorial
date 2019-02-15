6c6590a800198c8a00be7ae5180207781862a3fb
cyj407 <dfghj345678@gmail.com>
2019-02-15 15:08:23 +0800

    Add  folder and  scene

---
+++ Assets/Scripts/Tape_model.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Tape_model : VRTK_InteractableObject {
+
+    private VRTK_ControllerReference controllerReference;
+
+    public override void Grabbed(VRTK_InteractGrab grabbingObject)
+    {
+        base.Grabbed(grabbingObject);
+        controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+    }
+
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+        controllerReference = null;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        controllerReference = null;
+        interactableRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+}
