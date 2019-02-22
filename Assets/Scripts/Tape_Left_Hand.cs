b93a15ca59298c5ae050006dff40823777fe822b
cyj407 <dfghj345678@gmail.com>
2019-02-22 15:38:46 +0800

    Add  scene to test function

---
+++ Assets/Scripts/Tape_Left_Hand.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Tape_Left_Hand : VRTK_InteractableObject
+{
+    private VRTK_ControllerReference controllerReference;
+
+
+    public override void Grabbed(VRTK_InteractGrab grabbingObject)
+    {
+        base.Grabbed(grabbingObject);
+        controllerReference = VRTK_ControllerReference.GetControllerReference(grabbingObject.controllerEvents.gameObject);
+    }
+
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+        controllerReference = null;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        controllerReference = null;
+        interactableRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+
+    public override void OnInteractableObjectUsed(InteractableObjectEventArgs e)
+    {
+        base.OnInteractableObjectUsed(e);
+        Debug.Log(this.name);
+    }
+}
