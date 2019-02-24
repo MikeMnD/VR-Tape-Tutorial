c862c95a8265b5a2b636313af7d19cbd5f1084a9
cyj407 <dfghj345678@gmail.com>
2019-02-24 12:36:28 +0800

    Add Tape_length_controller.cs

---
+++ Assets/Scripts/Tape_length_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tape_length_controller : MonoBehaviour {
+
+    public Transform[] points;
+    public LineRenderer lineRederer;
+
+	// Use this for initialization
+	void Start () {
+        lineRederer.positionCount = points.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0;i < points.Length; ++i)
+        {
+            lineRederer.SetPosition(i, points[i].position);
+        }
+	}
+}
