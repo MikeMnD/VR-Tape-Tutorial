e5151a81efe049ae1c3892890d556057bbae5e89
cyj407 <dfghj345678@gmail.com>
2019-04-09 15:23:45 +0800

    Add the function of extend the tape

--- Assets/Scripts/AttachTapeInHands.cs
+++ Assets/Scripts/AttachTapeInHands.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class AttachTapeInHands : MonoBehaviour {
 
-    private SteamVR_TrackedObject trackedObject;
-    private SteamVR_Controller.Device device
-    {
-        get { return SteamVR_Controller.Input((int)trackedObject.index); }
-    }
+    public GameObject leftPosition, rightPosition;
+    private float init_dist;
+    private float init_len;
+    private Vector3 init_left_pos, init_right_pos;
 
-    private void Awake()
-    {
-        trackedObject = GetComponent<SteamVR_TrackedObject>();
-    }
     // Use this for initialization
     void Start () {
-		
-	}
+        init_dist = Vector3.Distance(leftPosition.transform.position, rightPosition.transform.position);
+        //Debug.Log(original_dist);  //original distance = 0.4
+        init_len = this.transform.localScale.x;
+        //Debug.Log(original_len);  //original length = 0.04
+        //init_left_pos = leftPosition.transform.position;
+        //init_right_pos = rightPosition.transform.position;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float dist = Vector3.Distance(leftPosition.transform.position, rightPosition.transform.position);
+        
+        
+        //Debug.Log(original_len * (dist / original_dist));
+        //Debug.Log(leftPosition.transform.position + "," +rightPosition.transform.position);
+        this.transform.localScale = new Vector3(init_len * (dist/ init_dist), 1.0f, 0.01f);
+        //Debug.Log(this.transform.position);
+    }
 }
