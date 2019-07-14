99b5e98c4747c266b82c71a27ce05b7eb0e3e99a
cyj407 <dfghj345678@gmail.com>
2019-07-14 09:05:45 +0800

    Add trail prefab and AnimControl.cs

---
+++ Assets/Animation/AnimControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+
+    void destroyTrail()
+    {
+        GameObject trail = GameObject.Find("trail(Clone)");
+        Destroy(trail);
+    }
+
+    void createTrail()
+    {
+        Object trailObj = Resources.Load("trail");
+        Instantiate(trailObj, this.transform);
+    }
+}
---
+++ Assets/Scripts/TrailRoute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailRoute : MonoBehaviour {
+
+    private TrailRenderer trailRenderer;
+
+	// Use this for initialization
+	void Start () {
+        trailRenderer.GetComponent<TrailRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
